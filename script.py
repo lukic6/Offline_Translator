28e57857f30f0a9df8c179fed42089f9f2f1c768
lukic6 <lkorencan@gmail.com>
2022-12-13 19:15:47 +0100

    changed FileNotFoundError message

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 from translate import Translator
 
-translator = Translator(to_lang="de")
+translator = Translator(to_lang="sl")
 
 try:
     with open("test.txt", mode="r") as my_file:
         text = my_file.read()
         translation = translator.translate(text)
-        with open("./test-de.txt", mode="w") as my_file2:
+        with open("./test-sl.txt", mode="w") as my_file2:
             my_file2.write(translation)
 except FileNotFoundError as e:
-    print("check your file path")
+    print("FileNotFoundError; Changing file path should resolve this issue :D")
